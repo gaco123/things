f88a3ddd0c8df25d0ca3edd35fe7e44e9a012d47
gaco123 <104175926+gaco123@users.noreply.github.com>
2023-09-17 17:10:29 -0500

    Update sus.py

--- sus.py
+++ sus.py
@@ -157,10 +157,10 @@ def git_clone(url, dir, name, commithash=None):
 def git_pull_recursive(dir):
     for subdir, _, _ in os.walk(dir):
         if os.path.exists(os.path.join(subdir, '.git')):
-            try:
-                output = subprocess.check_output([git, '-C', subdir, 'pull', '--autostash'])
+            #try:
+             output = subprocess.check_output([git, '-C', subdir, 'pull', '--autostash'])
                 #print(f"Pulled changes for repository in '{subdir}':\n{output.decode('utf-8').strip()}\n")
-            except subprocess.CalledProcessError as e:
+            #except subprocess.CalledProcessError as e:
                 #print(f"Couldn't perform 'git pull' on repository in '{subdir}':\n{e.output.decode('utf-8').strip()}\n")
 
 
